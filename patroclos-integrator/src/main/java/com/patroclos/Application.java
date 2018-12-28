edfb2230825944d4dad1b4fda1679d43b11107dd
Luiz Albuquerque <luiz.patroclos@gmail.com>
2018-12-28 10:47:33 -0200

    commit integrator

---
+++ patroclos-integrator/src/main/java/com/patroclos/Application.java
@@ -0,0 +1,17 @@
+package com.patroclos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.jms.annotation.EnableJms;
+
+@EnableDiscoveryClient	
+@SpringBootApplication
+@EnableJms
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
